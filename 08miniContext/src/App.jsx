2259eafeeb470790eda2cf88369ffb9194442dae
Ayush <ayushyadav4964@gmail.com>
2026-01-24 02:00:59 +0530

    miniContext

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
+import Login from './Components/login'
+import Profile from './Components/Profile'
+
+
+function App() {
+
+  return (
+    <UserContextProvider>
+    <h1>React with Ayush Yadav</h1>
+    <Login />
+    <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
